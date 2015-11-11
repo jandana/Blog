ba2177cc13f0b9239b3c5a7e7d5b3791d47e0084
Javier <tenienteDan123@gmail,com>
2015-11-11 16:20:03 -0300

    Se agrego autentificacion y proteccion de rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,23 +11,43 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['as'=>'admin.index',function(){
     return view('welcome');
-});
+}]);
 
-Route::group(['prefix'=>'admin'], function(){
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
+	
+	Route::get('/', ['as'=>'admin.index',function(){
+    	return view('welcome');
+	}]);
+
 	Route::resource('users','UsersController');
 	Route::get('users/{id}/destroy',[
-		'uses'=>'UsersController@destroy',
-		'as'=>'admin.users.destroy'
+		'uses'	=>'UsersController@destroy',
+		'as'	=>'admin.users.destroy'
 	]);
 
 	Route::resource('categories','CategoriesController');
 	Route::get('categories/{id}/destroy',[
-		'uses'=>'CategoriesController@destroy',
-		'as'=>'admin.categories.destroy'
+		'uses'	=>'CategoriesController@destroy',
+		'as'	=>'admin.categories.destroy'
 		]);
 });
+
+Route::get('admin/auth/login',[ 
+	'uses'	=>'Auth\AuthController@getLogin',
+	'as'	=>'admin.auth.login'
+]);
+
+Route::post('admin/auth/login',[ 
+	'uses'	=>'Auth\AuthController@postLogin',
+	'as'	=>'admin.auth.login'
+]);
+
+Route::get('admin/auth/logout',[ 
+	'uses'	=>'Auth\AuthController@getLogout',
+	'as'	=>'admin.auth.logout'
+]);
 
 
 
